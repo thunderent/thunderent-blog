f0e34ecff2bd129bd7f640bfe0b5f2ca66b9fecb
Nechitelea Robert <robert.nechitelea@toluna.com>
2020-02-13 16:42:12 +0200

    share to facebook - WIP 0.1v

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,3 +6,9 @@ export const getIdFromCustomURL = (url) => {
     console.log(url.indexOf("article"));
     return url.slice(url.indexOf("article")+8);
 }
+
+export const serializeArticleForShare = (params) => {
+    return Object.keys(params)
+    .map(param => `${param}=${encodeURIComponent(params[param].trim())}`)
+    .join("&");   
+}
